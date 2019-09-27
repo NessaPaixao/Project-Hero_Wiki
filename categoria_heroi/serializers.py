949674fdc06b7e7c7939f500009430f7814dc831
NessaPaixao <vanessadapaixaobarros1@gmail.com>
2019-09-27 18:11:28 -0300

    Adicionar habilidade e categoria serializers, commit do prof

--- categoria_heroi/serializers.py
+++ categoria_heroi/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers        
 from categoria_heroi.models import Categoria
 
 
--- habilidades/serializers.py
+++ habilidades/serializers.py
@@ -5,3 +5,12 @@ from habilidades.models import Habilidade
 class HabilidadeSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     nome = serializers.CharField()
+
+    def create(self, validated_data):
+        habilidade = Habilidade.objects.create(**validated_data)
+        return habilidade
+
+    def update(self, instance, validated_data):
+        instance.nome = validated_data.get('nome')
+        instance.save()
+        return instance
