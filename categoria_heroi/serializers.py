e6b5da13efd26e41debb8a3df30f6eaec3c282a7
NessaPaixao <vanessadapaixaobarros1@gmail.com>
2019-09-27 18:06:08 -0300

    Adicionar serializer Categoria

---
+++ categoria_heroi/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from categoria_heroi.models import Categoria
+
+
+class CategoriaSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    nome = serializers.CharField()
+
+    def create(self, validated_data):
+        categoria = Categoria.objects.create(**validated_data)
+        return categoria
+
+    def update(self, instance, validated_data):
+        instance.nome = validated_data.get('nome')
+        instance.save()
+        return instance
