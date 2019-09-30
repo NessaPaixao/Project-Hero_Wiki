6888b59c6d83b9a76e7a7650e1dfa8ae63f8e195
Gabriel Santos <gabrielsturaro2002@gmail.com>
2019-09-30 16:24:55 -0300

    Adicionar Settings para Banco de Dados

--- hero_wiki_project/settings.py
+++ hero_wiki_project/settings.py
@@ -83,7 +83,11 @@ WSGI_APPLICATION = 'hero_wiki_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.environ.get('HW_DB_NAME'),
+        'USER': os.environ.get('HW_DB_USER'),
+        'PASS': os.environ.get('HW_DB_PASS'),
+        'HOST': os.environ.get('HW_DB_HOST'),
+        'PORT': os.environ.get('HW_DB_PORT'),
     }
 }
 
@@ -125,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
