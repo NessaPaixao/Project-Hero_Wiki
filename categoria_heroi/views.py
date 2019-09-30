45a358957a73ac4fbdd0d48bfce7a3bad45d7c9a
Luana <luanamgs10@gmail.com>
2019-09-30 17:45:47 -0300

    Adicionar views de Categoria

--- categoria_heroi/views.py
+++ categoria_heroi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from categoria_heroi.models import Categoria
+from categoria_heroi.serializers import CategoriaSerializer
 
-# Create your views here.
+
+class CategoriaWiewSet(viewsets.ModelsViewSet):
+    filter_backends = [SearchFilter]
+    search_fields = ['nome']
+    queryset = Categoria.objects.all
+    serializer_class = CategoriaSerializer
