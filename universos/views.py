108203d79f266fb5152f35e4ad159c6b983ae079
Luana <luanamgs10@gmail.com>
2019-09-30 17:43:24 -0300

    Adicionar views de Universo

--- universos/views.py
+++ universos/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from universos.models import Universo
+from universos.serializers import UniversoSerializer
+
+
+class UniversoWiewSet(viewsets.ModelsViewSet):
+    filter_backends = [SearchFilter]
+    search_fields = ['nome']
+    queryset = Universo.objects.all
+    serializer_class = UniversoSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Create your views here.
