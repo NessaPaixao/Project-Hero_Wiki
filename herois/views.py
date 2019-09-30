6c9ee1cf866546c899d89d606fc26b91a935fa74
Luana <luanamgs10@gmail.com>
2019-09-30 17:40:22 -0300

    Adicionar views de Heroi

--- herois/views.py
+++ herois/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from herois.models import Heroi
+from herois.serializers import HeroiSerializer
 
-# Create your views here.
+
+class HeroisWiewSet(viewsets.ModelsViewSet):
+    filter_backends = [SearchFilter]
+    search_fields = ['nome']
+    queryset = Heroi.objects.all
+    serializer_class = HeroiSerializer
+
+def index(request):
+    herois = Heroi.objects.all()
+
+    contexto = {
+        'herois': herois
+    }
+
+    return render(request, '.html', contexto)
